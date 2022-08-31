fcb7457bfb43c76f8d56d622f0c47bd6724aa0ef
sherxonDeveloper <sherxontoshpulatov3@gmail.com>
2022-08-31 20:44:17 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+let arr = ["I","study", "JavaScript","right","now"]
+
+console.log(arr.slice(2,3));
+
+
+// 2-misol
+// let text = "Salom Dunyo"
+
+// return text.split("").reverse().join();
+
+
+// 3-misol
+// function son(a) {
+//   if (a % 2 == 0) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// }
+// console.log(son(41));
+
+
+// function son(a){
+//      if (a % 2 == 0) {
+//        console.log("juft son");
+//      } else {
+//        console.log("toq son");
+//      }
+// }
+//    console.log(son(42));
+
+
+//     let a = 12
+//     if(a % 2 == 0){
+//         console.log("juft son");
+//     }
+//     else {
+//         console.log("toq son");
+//     }
+
+
+
+
+
+
+
+
+
+
+
